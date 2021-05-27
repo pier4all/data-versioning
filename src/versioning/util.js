324d184a8785d9857b27ba4a2a590dc232346e73
Lucia de Espona <lucia.espona@gmail.com>
2021-05-27 09:14:22 +0200

    improved reporting

--- src/versioning/util.js
+++ src/versioning/util.js
@@ -42,14 +42,14 @@ exports.isValidVersion = (v) => {
 
   exports.logTimer = (report, tag, diff, input, collection) => {
     const time = `${(diff[0] * NS_PER_SEC + diff[1])/1e6}`
+    console.log(`TYPE: ${input.constructor.name}`)
 
-    if (Array.isArray(input.type)) 
-        fs.appendFileSync(report, [tag, collection, "0", new Date().toISOString(), input.length, time].join(sep) + '\n')
+    if (input.constructor.name === 'Array') 
+        fs.appendFileSync(report, [tag, collection, "0", new Date().toISOString(), input.length, time].join(SEP) + '\n')
     else {
         const bytesize = Buffer.from(JSON.stringify(input)).length
         fs.appendFileSync(report, [tag, collection, input._version, new Date().toISOString(), bytesize, time].join(SEP) + '\n')
     }
-
   }
 
   exports.paramList2object = (text) => {
