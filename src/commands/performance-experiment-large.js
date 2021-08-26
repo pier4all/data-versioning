9ce39fa4d0fdef877fec23b436eae93319ee53fb
Lucia de Espona <lucia.espona@gmail.com>
2021-08-27 00:33:25 +0200

    Added large experiment

---
+++ src/commands/performance-experiment-large.js
@@ -0,0 +1,223 @@
+var chalk = require('chalk');
+const axios = require('axios');
+var path = require('path');
+
+const config_path = __dirname+'/../../config/.env'
+require('dotenv').config({path: config_path})
+const { ObjectID } = require('mongodb');
+
+// Read input files
+'use strict';
+const fs = require('fs');
+
+const generateRequest = (collection, id, version, endpoint='crud') => {
+
+    var url = `${process.env.API_URL}:${process.env.PORT}/${endpoint}/${collection}`
+
+    if (id) {
+        if (version) return url + `/${id}/${version}`
+        return url + `/${id}`
+    }
+    else return url
+}
+
+async function wait(ms = 5000) {
+    return new Promise(resolve => {
+      setTimeout(resolve, ms);
+    });
+  }
+
+  function parseDocument(line) {
+
+    let document = JSON.parse(line)
+
+    return document
+  }
+
+const run = async () => {
+
+    // Batch 1M
+    var fileList = [
+        path.join(__dirname, 'data', 'employee.json')
+        //  path.join(__dirname, 'data', 'batch_1M', 'employee_100000.json'),  
+        //  path.join(__dirname, 'data', 'batch_1M', 'employee_200000.json'),  
+        //  path.join(__dirname, 'data', 'batch_1M', 'employee_300000.json'),  
+        //  path.join(__dirname, 'data', 'batch_1M', 'employee_400000.json'),  
+        //  path.join(__dirname, 'data', 'batch_1M', 'employee_500000.json'),  
+        //  path.join(__dirname, 'data', 'batch_1M', 'employee_600000.json'),  
+        //  path.join(__dirname, 'data', 'batch_1M', 'employee_700000.json'),  
+        //  path.join(__dirname, 'data', 'batch_1M', 'employee_800000.json'),  
+        //  path.join(__dirname, 'data', 'batch_1M', 'employee_900000.json'),  
+        //  path.join(__dirname, 'data', 'batch_1M', 'employee_1000000.json') 
+    ]
+
+    const COLLECTION = 'employee'
+
+    
+    for (var file of fileList) {
+        //read the docs
+        console.log(chalk.cyan.bold("\n * Reading file: " + file))
+
+        const readline = require('readline');
+
+        const fileStream = fs.createReadStream(file);
+
+        const rl = readline.createInterface({
+            input: fileStream,
+            crlfDelay: Infinity
+        });
+        // Note: we use the crlfDelay option to recognize all instances of CR LF
+        // ('\r\n') in input.txt as a single line break.
+        let documents = []
+
+        for await (const line of rl) {
+            // Each line in input.txt will be successively available here as `line`.
+            let document = parseDocument(line);
+            //console.log(`Doc from file: ${JSON.stringify(document)}`);
+            documents.push(document)
+        }
+
+        console.log("\t - Read " + documents.length + " documents");
+
+        var inserted = []
+
+        // insert them
+        var collection = COLLECTION 
+        console.log("\t - Inserting at Collection '" + collection + "'");
+        var url = generateRequest(collection)
+        for(var document of documents) {
+            try {
+                // only for documents with dates or ids (fix for mongoose)
+                Object.keys(document).forEach(function(key) {
+                    var value = document[key]
+                    var tags = ["$oid", "$date"]
+                    tags.forEach( tag => {
+                        if ((!!value) && (value.constructor === Object) 
+                                      && (value.hasOwnProperty(tag))) {
+                            document[key] = value[tag]
+                        }
+                    })
+                }); 
+                // ensure unique email
+                if (document.email) {
+                    document.email = document.email.split('@')[0] + document.custno + '@' + document.email.split('@')[1]
+                }
+                // post the insert
+                var response = await axios.post(url, document);     
+                inserted.push(response.data) 
+
+            } catch (error) {
+                console.error(chalk.redBright.bold(error.message));
+                return
+            }           
+        }
+        
+        await wait()
+
+        console.log("\t - Updating at Collection '" + collection + "'");
+        for(var document of inserted) {
+            await wait(100)
+            try {
+                // TODO for other types of documents
+                var updated_document = { name: 'New '+ document.name }
+                let id = document._id._id || document._id
+                url = generateRequest(collection, id, 1)
+                var response = await axios.patch(url, updated_document);     
+            } catch (error) {
+                console.error(chalk.redBright.bold(error.message));
+                console.error(chalk.red(error));
+                return
+            }           
+        }
+
+        await wait()
+
+        console.log("\t - Querying currently valid version at Collection '" + collection + "'");
+        for(var document of inserted) {
+            try {
+                let id = document._id._id || document._id
+                url = generateRequest(collection, id)
+                var response = await axios.get(url);     
+            } catch (error) {
+                console.error(chalk.redBright.bold(error.message));
+                return
+            }           
+        }
+
+        await wait()
+
+        console.log("\t - Querying past valid version at Collection '" + collection + "'");
+        for(var document of inserted) {
+            try {
+                if (document._validity) {
+                    var date = document._validity.start
+                    let id = document._id._id || document._id
+                    url = generateRequest(collection, id)
+                    var response = await axios.get(url, {params: {date}});    
+                } 
+            } catch (error) {
+                console.error(chalk.redBright.bold(error.message));
+                return
+            }           
+        }
+
+        await wait()
+
+        console.log("\t - Querying current version at Collection '" + collection + "'");
+        for(var document of inserted) {
+            try {
+                let id = document._id._id || document._id
+                url = generateRequest(collection, id)
+                var response = await axios.get(url + "/2");     
+            } catch (error) {
+                console.error(chalk.redBright.bold(error.message));
+                return
+            }           
+        }
+
+        await wait()
+
+        console.log("\t - Querying previous version at Collection '" + collection + "'");
+        for(var document of inserted) {
+            try {
+                let id = document._id._id || document._id
+                url = generateRequest(collection, id)
+                var response = await axios.get(url + "/1");     
+            } catch (error) {
+                console.error(chalk.redBright.bold(error.message));
+                return
+            }           
+        }
+
+        await wait()
+
+        console.log("\t - Find by non indexed field at current collection '" + collection + "'");
+        for(var document of inserted) {
+            try {
+                url = generateRequest(collection, 'find', undefined, endpoint='query')
+                url += `?query="_validity.end": null,"pricePerWorkingUnit": {"$lte": ${document.pricePerWorkingUnit}}`
+                var response = await axios.get(url); 
+            } catch (error) {
+                console.error(chalk.redBright.bold(error.message));
+                return
+            }           
+        }
+
+        await wait()
+
+        console.log("\t - Deleting from Collection '" + collection + "'");
+        for(var document of inserted) {
+            try {
+                let id = document._id._id || document._id
+                url = generateRequest(collection, id)
+                var response = await axios.delete(url + '/2');     
+            } catch (error) {
+                console.error(chalk.redBright.bold(error.message));
+                return
+            }           
+        }
+    } 
+}
+
+
+run()
--- src/models/employee.js
+++ src/models/employee.js
@@ -1,6 +1,6 @@
 // imports
 const mongoose = require('mongoose')
-// const versioning = require('mongoose-versioned/source/versioning')
+//const versioning = require('mongoose-versioned/source/versioning')
 mongoose.Promise = require('bluebird')
 
 const NAME = "employee"
@@ -9,14 +9,107 @@ const NAME = "employee"
 let Schema = mongoose.Schema
 
 let employeeSchema = new Schema({
-  empno: { type: Number, required: true, unique: true },
-  svn : { type : String, required: true, unique: true },
-  lastname: { type: String, required: true },
-  firstname: { type: String, required: true },
-  pensum: { type: String, required: true, default: "100%"}
+    supervisor: {type: mongoose.Schema.Types.ObjectId, ref: "Employee"},
+    projects: [
+        {
+        projectID: {type: mongoose.Schema.Types.ObjectId, ref: "Project"},
+        projectStart: {type: Date},
+        projectEnd: {type: Date},
+        projectActive: {type: Boolean}
+        } 
+    ],
+    pricePerWorkingUnit: {type: Number},
+    validFrom: [{type: Date}],
+    validUntil: [{type: Date}],
+    active: {type: Boolean, default: true},
+    user: {
+        role: {type: mongoose.Schema.Types.ObjectId, ref: "Role", required:true},
+        username: {type: String, required: true},
+        active: {type: Boolean, default: true} },
+    person: {
+        firstName: {type: String, required: true, index: true},
+        lastName: {type: String, required: true, index: true},
+        dateOfBirth: {type: Date, required: true},
+        AHVnumber: {type: String, required: true},
+        phoneNumber: {type: String, required: true},
+        phoneNumberPrefix: {type: String, required: true},
+        personalEmail: {type: String, required: true},
+        workEmail: {type: String},
+        usedHolidays: {type: Number},
+        totalHolidays: {type: Number},
+        salaryFulltime: {type: Number, required: true},
+        levelOfEmployment: {type: Number, required: true},
+        address: {
+            street: {type: String, required: true},
+            PLZ: {type: String, required: true},
+            canton: {type: String, required: true},
+            country: {type: String, required: true},
+        },
+        bank: {
+            bankName: {type: String, required: true},
+            bankAdress: {type: String, required: true},
+            bankPLZ: {type: String, required: true},
+            SWIFT: {type: String, required: true},
+            IBAN: {type: String, required: true},
+            currency: {
+                type: String,
+                enum: ["CHF", "EUR", "USD"],
+                default: "CHF"}   
+        },
+        children: [{
+            _id: false,
+            name: {type: String},
+            dateOfBirth: {type: Date, required: true},
+        }],
+        holidays: [{
+            holidayFrom: {type: Date, required: true},
+            holidayUntil: {type: Date, required: true},
+            holidayApproved: {type: Boolean, required: true},
+    
+        }],
+        skills: [{
+            _id: false,
+            skillName: {type: String, required: true},
+            skillDescription: {type: String, required: true},
+            skillCertificate: {
+                file: {type: Buffer},
+                filename: {type: String},
+                mimetype: {type: String}
+            },
+            skillGroup: {type: String},
+            skillTags: [{type: String, index: true }]
+        }],
+        training: [{
+            _id: false,
+            trainingName: {type: String, required: true},
+            trainingApproved: {type: Boolean, required: true},
+            trainingCost: {type: Number, required: true},
+            trainingDateRequest: {type: Date, required: true},
+            trainingDateFrom: {type: Date, required: true},
+            trainingDateUntil: {type: Date, required: true},
+            trainingAttachments: [{
+                file: {type: Buffer},
+                filename: {type: String},
+                mimetype: {type: String}
+            }],
+            trainingCertificates: [{
+                file: {type: Buffer},
+                filename: {type: String},
+                mimetype: {type: String}
+            }],
+            trainingProviders: [{
+                _id: false,
+                providerName: {type: String, required: true},
+                providerAddress: {type: String, required: true},
+                providerPLZ: {type: String, required: true},
+                providerCity: {type: String, required: true},
+                providerCountry: {type: String, required: true},
+            }]
+        }]
+    }
 })
 
 // TODO set indexes manually after adding option { autoIndex: false }
-// employeeSchema.plugin(versioning, NAME + "s.versioning");
+// employeeSchema.plugin(versioning, {collection: NAME + "s.versioning", mongoose})
 
 module.exports = mongoose.model(NAME, employeeSchema)
